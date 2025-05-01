d9bb074d85038dd1c65b2ace14873b095fe13499
Raj Bobra <rajbobra.py@gmail.com>
2025-05-01 15:12:55 -0700

    Added copy assignments, destructor and overloaded deref/ref operators

--- main.cc
+++ main.cc
@@ -1,3 +1,29 @@
-#pragma once
+#include <iostream>
+#include "ref_ptr.hh"
 
-// Testing code will go here
+
+class TestClass {
+    public:
+        TestClass() { std::cout << "Test Class created\n"; }
+        ~TestClass() { std::cout << "Test Class Destroyed\n"; }
+
+        void print() {
+            std::cout << "Test class printed something!\n"; 
+        }
+};
+
+
+int main() {
+    RefPtr<TestClass> primaryPtr(new TestClass());
+    primaryPtr->print();
+
+    {
+        RefPtr<TestClass> copyPtr = primaryPtr;
+        copyPtr->print();
+        std::cout << "Inside scope, use count: " << copyPtr.use_count() << std::endl;
+    }
+
+    std::cout << "Outside scope, use count: " << primaryPtr.use_count() << std::endl;
+
+    return 0;
+}
--- ref_ptr.hh
+++ ref_ptr.hh
@@ -6,24 +6,66 @@ class RefPtr {
         struct ControlBlock {
             int ref_count;
             T* ptr;
-        }
+
+            ControlBlock(T* raw) : ptr(raw), ref_count(1) {}
+        };
 
         ControlBlock* control;
 
     public:
         // constructor
-        explict RefPtr(T* rawPtr);
+        explicit RefPtr(T* rawPtr){
+            this->control = new ControlBlock(rawPtr);
+        }
+
         // copy constructor
-        RefPtr(const RefPtr& other);
+        RefPtr(const RefPtr& other) {
+            this->control = other.control;
+            this->control->ref_count++;
+        }
+
+        // move
+        RefPtr(RefPtr&&) noexcept;
+
         // copy assignment operator
-        RefPtr& operator=(const RefPtr& other);
+        RefPtr& operator=(const RefPtr& other) {
+            if(this != &other) {
+                this->control->ref_count--;
+                if(this->control->ref_count == 0) {
+                    delete this->control->ptr;
+                    delete this-> control;
+                }
+
+                this->control = other.control;
+                this->control->ref_count++;
+            }
+
+            return *this;
+        }
+
+        // move assignment operator
+        RefPtr& operator=(const RefPtr&& other);
+
         // destructor
-        ~RefPtr();
+        ~RefPtr() {
+            this->control->ref_count--;
+            if(this->control->ref_count == 0) {
+                delete this->control->ptr;
+                delete this-> control;
+            }
+        }
+
+        T* operator->() const {
+            return this->control->ptr;
+        }
 
         // dereference operator
-        T& operator*() const;
-        T* operator->() const;
+        T& operator*() const {
+            return *(this->control->ptr);
+        }
 
         // get the number of references for debugging
-        int use_count() const;
-}
+        int use_count() const {
+            return this->control->ref_count;
+        }
+};
